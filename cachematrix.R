0eb2d1ea57c1fd85b794ac8c36df32f46d3b9314
renaudrioual <renaud.rioual.dump@gmail.com>
2016-09-13 13:00:05 +0200

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## code pour la cachematrice et la cachesolve !! 
 
-## Write a short comment describing this function
 
+##  This function sets a matrix in the enviroment, then when the inverse is called, caches the result
 makeCacheMatrix <- function(x = matrix()) {
+	m <- NULL  ## could not find m unless was initialized to NULL
+	set <- function(y) {
+		x <<- y
+		m <<- NULL
+	}
+	get <- function() x
+	## switched mean for inverse from the vector function
+	setinverse <- function(inverse) m <<- inverse
+	getinverse <- function() m
 
+	list(set = set, get = get,
+		 setinverse = setinverse,
+		 getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+
+
+## fonction de check
+## This functions check if the cached result (the matrix inverse) is already stored, if not null, then returns the cached value
+##    If the value is not cached, then inverse the cached original matrix (using the solve()) then set the cached value for retrival later
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+	m <- x$getinverse()
+	
+     if(!is.null(m)) {
+          message("getting cached data")
+          return(m)
+     }
+	 
+     data <- x$get()
+     m <- solve(data, ...)
+     x$setinverse(m)
+     m
 }
+
